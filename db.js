be9d6af4148cd8cdd13dfe1d9486d484a45d3f0c
Pawan Gola <pawangola908@gmail.com>
2024-02-02 15:54:59 +0530

    node_modules be gone!

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const { default: mongoose } = require("mongoose")
 
-const mongoURL = 'mongodb://localhost:27017/hotels'
+
+//const mongoURL = process.env.MONGODB_URL_LOCAL
+const mongoURL = process.env.MONGODB_URL;
 
 // set up MongoDB connection 
 
--- server.js
+++ server.js
@@ -11,8 +11,12 @@ const express = require('express')
 const app = express();
 
 const db = require('./db');
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body  
+const PORT = process.env.PORT || 3000;
+
 
 app.get('/', function (req, res) {
    res.send('Welcome to my hotel..How i can hey you ?. ')
@@ -28,6 +32,7 @@ app.use('/person', personRoutes);
 app.use('/menu',menuitemRoutes)
 
 //listen  at 3000 port 
+
 app.listen(3000, () => {
    console.log('Listening on port 3000');
 })
